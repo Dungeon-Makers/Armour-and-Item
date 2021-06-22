5b47d84b27720eada51ecdccb422df9d8d49c56f
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-22 16:16:01 +0100

    Update DataGenerators.java

---
+++ src/main/java/com/yusuf/armouranditem/data/DataGenerators.java
@@ -0,0 +1,11 @@
+package com.yusuf.armouranditem.data;
+
+
+import com.yusuf.armouranditem.ArmourAndItem;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ArmourAndItem.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    private DataGenerators() {}
+
+}
